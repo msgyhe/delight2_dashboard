7da7f46c69b5017812a8f47db16e082544206558
msgyhe <34791217+msgyhe@users.noreply.github.com>
2025-12-23 21:10:05 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
+    // Vercel 환경 변수를 브라우저에서 사용할 수 있도록 주입
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
